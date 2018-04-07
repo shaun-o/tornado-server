1421225f8df580152e735e31733b87e9d3c9ed3d
Shaun Oates <shaun_o@me.com>
2018-04-07 14:41:07 +0100

    Reading from blockchain

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from tornado.tcpserver import TCPServer
 import tornado.ioloop
 import tornado.web
 import wtforms
+import blockchain.blockexplorer
 from wtforms_tornado import Form
 
 
@@ -40,6 +41,28 @@ class SimpleForm(tornado.web.RequestHandler):
             self.set_status(400)
             self.write(form.errors)
 
+class BlockchainAddy(Form):
+    address = wtforms.TextField(
+        'Address', validators=[wtforms.validators.DataRequired()], default=u'')
+
+class BlockchainQuery(tornado.web.RequestHandler):
+    def get(self):
+        form = BlockchainAddy()
+        loader = tornado.template.Loader("templates")
+        template = loader.load("blockchainaddy.html")
+        self.write(template.generate(form=form))
+
+    def post(self):
+        form = BlockchainAddy(self.request.arguments)
+        details = ''
+        if form.validate():
+            for f in self.request.arguments:
+                details += self.get_argument(f, default=None, strip=False)
+            address = blockchain.blockexplorer.get_address(details)
+            self.write(str(address))
+        else:
+            self.set_status(400)
+            self.write(form.errors)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -57,6 +80,7 @@ class PageHandler(tornado.web.RequestHandler):
 application = tornado.web.Application([
     (r"/", MainHandler),
     (r"/simple", SimpleForm),
+    (r"/blockchain_addy", BlockchainQuery),
     (r"/page", PageHandler)
 ])
 
